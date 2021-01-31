a96e21ab9a659e24aad4d62a4c815497e6702d49
lufia <lufia@lufia.org>
2021-01-31 17:58:19 +0900

    enable esModuleInterop option

--- src/highlight.ts
+++ src/highlight.ts
@@ -1,7 +1,7 @@
 // @ts-ignore /lib/highlight hasn't type definitions.
-import * as hljs from "highlight.js/lib/core";
+import hljs from "highlight.js/lib/core";
 // @ts-ignore /lib/languages/c hasn't type definitions.
-import * as c from "highlight.js/lib/languages/c";
+import c from "highlight.js/lib/languages/c";
 
 hljs.registerLanguage("c", c);
 export default hljs;
