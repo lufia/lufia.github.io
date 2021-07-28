1f0f7e0a3d3e1230ef19358147d20aa901494d84
lufia <lufia@lufia.org>
2021-07-29 07:00:27 +0900

    implement functions to generate HTML pages

---
+++ src/html-generator/index.ts
@@ -0,0 +1,58 @@
+import { ChildProcess, spawn } from "child_process";
+import { Transform } from "stream";
+
+export function include(dir?: string): Transform {
+	const args = dir ? ["-a", dir] : [];
+	const p = spawn("include", args, {
+		stdio: ["pipe", "pipe", "inherit"],
+	});
+	return createTransform(p);
+}
+
+type ConverterOptions = Readonly<{
+	lang: string;
+	extensions?: { [key: string]: string };
+	// TODO: fragment: boolean; will add -p option
+}>;
+
+export function convertToHtml(options: ConverterOptions): Transform {
+	const args: string[] = [];
+	if(options.lang)
+		args.push("-l", options.lang);
+	if(options.extensions)
+		for(const [key, value] of Object.entries(options.extensions))
+			args.push("-x", key, value);
+	const p = spawn("wf", args, {
+		stdio: ["pipe", "pipe", "inherit"],
+	});
+	return createTransform(p);
+}
+
+function createTransform(p: ChildProcess): Transform {
+	const que: string[] = [];
+	p.stdout.on("data", s => {
+		que.push(s);
+	});
+
+	const t = new Transform({
+		transform: (data, encoding, callback): void => {
+			p.stdin.write(data);
+			while(que.length > 0)
+				t.push(que.shift());
+			callback();
+		},
+		final: async (callback): Promise<void> => {
+			p.stdin.end();
+			const status = await new Promise((resolve, reject) => {
+				p.on("close", resolve);
+			});
+			if(status !== 0){
+				throw new Error(`${p.spawnfile}: exit with ${status}`);
+			}
+			while(que.length > 0)
+				t.push(que.shift());
+			callback();
+		},
+	});
+	return t;
+}
