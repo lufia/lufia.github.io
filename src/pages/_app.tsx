d645f2d345d034754841ffe02505605b28b1ad28
lufia <lufia@lufia.org>
2021-07-31 14:24:17 +0900

    import highlight.js

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,9 @@
 import type { AppProps } from "next/app";
 import "../styles.css";
+import hljs from "../highlight";
+
+if(process.browser)
+	hljs.initHighlightingOnLoad();
 
 function App({ Component, pageProps }: AppProps) {
 	return <Component {...pageProps} />;
