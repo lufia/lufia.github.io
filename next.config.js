c1de5b63a179fdfc3458bc05c5a3f6b47a5f9f05
lufia <lufia@lufia.org>
2021-08-12 05:28:23 +0900

    revert 12bf790

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 // FIXME: update NextJS and remove this workaround.
 // https://github.com/vercel/next.js/issues/12212
 
-// FIXME: if we can use next 11.0.2 or later, it supports ESM.
-const withTM = require("next-transpile-modules")([
-	"react-children-utilities"
-]);
-
-module.exports = withTM({
+module.exports = {
 	async rewrites() {
 		return [
 			{
@@ -15,4 +10,4 @@ module.exports = withTM({
 			},
 		];
 	},
-});
+};
