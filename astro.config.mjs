678f1f7f0a539079a850e9d62d68195aea4e74ac
lufia <lufia@lufia.org>
2024-02-04 14:29:29 +0900

    build.format = preserve

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,8 @@ import { defineConfig } from 'astro/config';
 import wf from './src/integrations/wf';
 
 export default defineConfig({
+	build: {
+		format: 'preserve'
+	},
 	integrations: [wf()]
 });
