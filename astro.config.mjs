083c6f6927b350e7426748227dff43d38bfa89f3
lufia <lufia@lufia.org>
2024-02-08 20:50:45 +0900

    add site to astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import wf from './src/integrations/wf';
 
 export default defineConfig({
+	site: 'https://lufia.org',
 	build: {
 		format: 'preserve'
 	},
